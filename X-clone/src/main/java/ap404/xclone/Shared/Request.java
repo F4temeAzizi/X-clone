5b53b782179c7cbc894ad679eed859cc0c04c979
Fateme Azizi <azizifateme423@gmail.com>
2026-07-01 01:19:01 +0330

    feat: implement Request.java

--- X-clone/src/main/java/ap404/xclone/Shared/Request.java
+++ X-clone/src/main/java/ap404/xclone/Shared/Request.java
@@ -1,4 +1,15 @@
 package ap404.xclone.Shared;
 
 public class Request {
+
+    private final RequestType type;
+    private final Object body;
+
+    public Request(RequestType type, Object body) {
+        this.type = type;
+        this.body = body;
+    }
+
+    public RequestType getType() { return type;}
+    public Object getBody() { return body; }
 }
