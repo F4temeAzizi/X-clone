5616a8a4a2efc2f0d0d3e7820bfc4c72985a4d1d
Fateme Azizi <azizifateme423@gmail.com>
2026-06-30 23:54:24 +0330

    refactor: rename MessageType to RequestType

--- X-clone/src/main/java/ap404/xclone/Shared/RequestType.java
+++ X-clone/src/main/java/ap404/xclone/Shared/RequestType.java
@@ -2,7 +2,7 @@ package ap404.xclone.Shared;
 
 import java.io.Serializable;
 
-public enum MessageType implements Serializable {
+public enum RequestType implements Serializable {
     LOGIN,
     LOGIN_SUCCESS,
     LOGIN_FAILED
