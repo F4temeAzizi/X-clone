8a7947648b1bea75c7b7121613a4c10f58681cca
Bita <bitahajati1386@gmail.com>
2026-06-30 22:42:08 +0330

    feat(signup): add signup.fxml

--- X-clone/src/main/java/ap404/xclone/Client/XApplication.java
+++ X-clone/src/main/java/ap404/xclone/Client/XApplication.java
@@ -2,7 +2,6 @@ package ap404.xclone.Client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
