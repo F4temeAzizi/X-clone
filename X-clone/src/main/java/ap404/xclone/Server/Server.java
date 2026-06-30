4343c85c3e11977bd95096836ce27b59a788402d
Fateme Azizi <azizifateme423@gmail.com>
2026-06-30 21:46:37 +0330

    feat: implement basic multi-threaded server

--- X-clone/src/main/java/ap404/xclone/Server/Server.java
+++ X-clone/src/main/java/ap404/xclone/Server/Server.java
@@ -1,4 +1,33 @@
 package ap404.xclone.Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private final int port = 5000;
+
+    public void start() throws IOException {
+
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+
+            while (!serverSocket.isClosed()) {
+
+                try {
+                    Socket socket = serverSocket.accept();
+
+                    ClientHandler clientHandler = new ClientHandler(socket);
+                    Thread thread = new Thread(clientHandler);
+                    thread.start();
+                }
+                catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
