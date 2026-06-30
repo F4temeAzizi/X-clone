b717620fe76d68c4d5bfcc02e8551b23393c1165
Bita <bitahajati1386@gmail.com>
2026-07-01 00:11:37 +0330

    feat(signup): implement SignupController.java"

--- X-clone/src/main/java/ap404/xclone/Client/Controllers/SignupController.java
+++ X-clone/src/main/java/ap404/xclone/Client/Controllers/SignupController.java
@@ -1,4 +1,35 @@
 package ap404.xclone.Client.Controllers;
 
-public class SignupController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import java.io.IOException;
+
+public class SignupController
+{
+    @FXML private TextField nameField;
+    @FXML private TextField usernameField;
+    @FXML private TextField emailField;
+    @FXML private PasswordField passwordField;
+    @FXML private PasswordField confirmPasswordField;
+
+    @FXML
+    private void handleSignup()
+    {
+        if (nameField.getText().isBlank()
+                || usernameField.getText().isBlank()
+                || emailField.getText().isBlank()
+                || passwordField.getText().isBlank()
+                || confirmPasswordField.getText().isBlank()) {
+            return;
+        }
+    }
+
+    @FXML
+    private void goToLogin() throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        nameField.getScene().setRoot(root);
+    }
 }
