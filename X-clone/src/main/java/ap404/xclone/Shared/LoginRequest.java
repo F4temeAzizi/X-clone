4fc99f31352dea978792977cf50d53147431b887
Fateme Azizi <azizifateme423@gmail.com>
2026-07-01 01:24:08 +0330

    feat: implement LoginRequest.java

--- X-clone/src/main/java/ap404/xclone/Shared/LoginRequest.java
+++ X-clone/src/main/java/ap404/xclone/Shared/LoginRequest.java
@@ -1,4 +1,18 @@
 package ap404.xclone.Shared;
 
-public class LoginRequest {
+import java.io.Serializable;
+
+public class LoginRequest implements Serializable {
+
+    private String username;
+    private String password;
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+
 }
