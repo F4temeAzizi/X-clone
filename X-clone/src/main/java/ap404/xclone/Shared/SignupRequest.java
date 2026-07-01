ce44041b215e890197d3f6642a09a0c5d8f83dda
Fateme Azizi <azizifateme423@gmail.com>
2026-07-02 00:08:30 +0330

    feat: add name to SignupRequest.java

--- X-clone/src/main/java/ap404/xclone/Shared/SignupRequest.java
+++ X-clone/src/main/java/ap404/xclone/Shared/SignupRequest.java
@@ -4,16 +4,19 @@ import java.io.Serializable;
 
 public class SignupRequest implements Serializable {
 
+    private String name;
     private String username;
     private String password;
     private String email;
 
-    public SignupRequest(String username, String password, String email) {
+    public SignupRequest(String name, String username, String password, String email) {
+        this.name = name;
         this.username = username;
         this.password = password;
         this.email = email;
     }
 
+    public String getName() { return name; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getEmail() { return email; }
