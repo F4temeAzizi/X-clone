e22e471cb4db5361437b27dd612dca00be1878c9
Fateme Azizi <azizifateme423@gmail.com>
2026-07-01 20:39:07 +0330

    feat: implement Client.java

--- X-clone/src/main/java/ap404/xclone/Client/Client.java
+++ X-clone/src/main/java/ap404/xclone/Client/Client.java
@@ -1,4 +1,33 @@
 package ap404.xclone.Client;
 
+import ap404.xclone.Shared.Request;
+import ap404.xclone.Shared.Response;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client {
+
+    private final int port = 5000;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public Client() throws IOException {
+
+        socket = new Socket("localhost", port);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+
+    }
+
+    public void sendRequest(Request request) throws IOException {
+        out.writeObject(request);
+        out.flush();
+    }
+
+    public Response getResponse() throws IOException, ClassNotFoundException {
+        return (Response) in.readObject();
+    }
 }
