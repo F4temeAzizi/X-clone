f9239f5c31314b2d8f5d08b9f0053a26fb771557
Fateme Azizi <azizifateme423@gmail.com>
2026-07-02 00:06:24 +0330

    feat: update handleSignup

--- X-clone/src/main/java/ap404/xclone/Client/Controllers/SignupController.java
+++ X-clone/src/main/java/ap404/xclone/Client/Controllers/SignupController.java
@@ -1,6 +1,8 @@
 package ap404.xclone.Client.Controllers;
 
+import ap404.xclone.Client.Client;
 import ap404.xclone.Client.MessageUtil;
+import ap404.xclone.Shared.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,13 +21,45 @@ public class SignupController
     @FXML
     private void handleSignup()
     {
-        if (nameField.getText().isBlank()
-                || usernameField.getText().isBlank()
-                || emailField.getText().isBlank()
-                || passwordField.getText().isBlank()
-                || confirmPasswordField.getText().isBlank()) {
+
+        String name = nameField.getText();
+        String username = usernameField.getText();
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+
+        if (name.isBlank()
+                || username.isBlank()
+                || email.isBlank()
+                || password.isBlank()
+                || confirmPassword.isBlank()) {
             MessageUtil.setErrorMessage(errorLabel, "All fields are required!");
             return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            MessageUtil.setErrorMessage(errorLabel, "Passwords do not match!");
+            return;
+        }
+
+        try {
+
+            Client client = new Client();
+
+            SignupRequest signupRequest = new SignupRequest(name, username, password, email);
+            client.sendRequest(new Request(RequestType.SIGNUP, signupRequest));
+
+            Response response = client.getResponse();
+
+            if (response.getType().equals(ResponseType.SIGNUP_SUCCESS)) {
+                MessageUtil.setErrorMessage(errorLabel, "Sign up succeed!");
+            }
+            else {
+                MessageUtil.setErrorMessage(errorLabel, "Sign up failed!");
+            }
+
+        } catch (Exception e) {
+            MessageUtil.setErrorMessage(errorLabel, "Cannot connect to server!");
         }
     }
 
