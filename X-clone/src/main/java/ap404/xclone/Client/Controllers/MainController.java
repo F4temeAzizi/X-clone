53421be09b8cb829fc4b0166634e8c3ebee16718
Bita <bitahajati1386@gmail.com>
2026-07-03 16:28:41 +0330

    feat(main): add a simple MainController.java

---
+++ X-clone/src/main/java/ap404/xclone/Client/Controllers/MainController.java
@@ -0,0 +1,47 @@
+package ap404.xclone.Client.Controllers;
+
+import ap404.xclone.Shared.Models.Tweet;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class MainController
+{
+    @FXML private VBox tweetContainer;
+
+    @FXML
+    public void initialize()
+    {
+        try
+        {
+            addTweet(new Tweet("Ali", "@ali", "Hello World!"));
+
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+            addTweet(new Tweet("Sara", "@sara", "Second Tweet!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void addTweet(Tweet tweet) throws Exception
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Tweet.fxml"));
+
+        HBox root = loader.load();
+
+        TweetController controller = loader.getController();
+        controller.setTweet(tweet);
+
+        tweetContainer.getChildren().add(root);
+    }
+}
