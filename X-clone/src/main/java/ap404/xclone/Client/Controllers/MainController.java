63462bd1c2b2b39bd87be69b7d9e0fec8313b413
Bita <bitahajati1386@gmail.com>
2026-07-04 22:17:49 +0330

    refactor(main): redesign UI navigation architecture
    extract home page into home.fxml
    
    add navigation manager
    
    add HomeController
    
    move home styles from main.css to home.css

--- X-clone/src/main/java/ap404/xclone/Client/Controllers/MainController.java
+++ X-clone/src/main/java/ap404/xclone/Client/Controllers/MainController.java
@@ -1,38 +1,17 @@
 package ap404.xclone.Client.Controllers;
-
-import ap404.xclone.Shared.Models.Tweet;
+import ap404.xclone.Client.Managers.Navigation;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+
 
 public class MainController
 {
-    @FXML private VBox tweetContainer;
+    @FXML private StackPane center;
 
     @FXML
     public void initialize()
     {
-        try
-        {
-            addTweet(new Tweet("Ali", "@ali", "Hello World!"));
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    private void addTweet(Tweet tweet) throws Exception
-    {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Tweet.fxml"));
-
-        HBox root = loader.load();
-
-        TweetController controller = loader.getController();
-        controller.setTweet(tweet);
-
-        tweetContainer.getChildren().add(root);
+        Navigation.setCenter(center);
+        Navigation.loadHome();
     }
 }
---
+++ X-clone/src/main/java/ap404/xclone/Client/Managers/Navigation.java
@@ -0,0 +1,32 @@
+package ap404.xclone.Client.Managers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class Navigation
+{
+    private static StackPane center;
+
+    public static void loadHome() { load("home.fxml"); }
+
+    public static void setCenter(StackPane centerPane)
+    {
+        center = centerPane;
+    }
+
+    public static void load(String fxml)
+    {
+        try
+        {
+            Parent root = FXMLLoader.load(Navigation.class.getResource("/" + fxml));
+            center.getChildren().setAll(root);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
