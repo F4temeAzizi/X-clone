f802b9f5f9a288fde1021a076ddf01b4d0a6d5d6
Fateme Azizi <azizifateme423@gmail.com>
2026-07-07 22:20:10 +0330

    feat: handle logout

--- src/main/java/ap404/xclone/Client/Controllers/MainController.java
+++ src/main/java/ap404/xclone/Client/Controllers/MainController.java
@@ -1,17 +1,29 @@
 package ap404.xclone.Client.Controllers;
 import ap404.xclone.Client.Managers.Navigation;
 import ap404.xclone.Client.Utils.UserUtil;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
+import java.io.IOException;
+
 public class MainController
 {
+    @FXML private Button moreOptionsBtn;
     @FXML private StackPane center;
     @FXML private ImageView sidebarAvatar;
     @FXML private Label sidebarName;
     @FXML private Label sidebarUsername;
+
+    private ContextMenu moreOptionsMenu;
 
     @FXML
     public void initialize()
@@ -20,6 +32,20 @@ public class MainController
         Navigation.setMainController(this);
         updateUserProfile();
         Navigation.loadHome();
+
+        moreOptionsMenu = new ContextMenu();
+        moreOptionsMenu.getStyleClass().add("x-menu");
+        MenuItem logoutItem = new MenuItem("Log Out");
+
+        logoutItem.setOnAction(e -> {
+            try {
+                Navigation.navigate("login.fxml");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        });
+
+        moreOptionsMenu.getItems().add(logoutItem);
     }
 
     @FXML public void goToProfile() { Navigation.loadProfile(); }
@@ -35,6 +61,15 @@ public class MainController
     @FXML public void  goToChat() {}
 
     @FXML public void goToSettings() {}
+
+    @FXML public void showMoreOptions(ActionEvent e) {
+
+        if(moreOptionsMenu.isShowing())
+            moreOptionsMenu.hide();
+        else {
+            moreOptionsMenu.show(moreOptionsBtn, Side.TOP, 0, 0);
+        }
+    }
 
     public void updateUserProfile ()
     {
