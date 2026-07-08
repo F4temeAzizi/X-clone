bb1f50dad1706bb46c4d50e6b3612bccf60236f7
Bita <bitahajati1386@gmail.com>
2026-07-08 20:16:11 +0330

    feat(explore): add ExploreController.java

---
+++ src/main/java/ap404/xclone/Client/Controllers/ExploreController.java
@@ -0,0 +1,26 @@
+package ap404.xclone.Client.Controllers;
+
+import ap404.xclone.Client.Utils.TweetUtil;
+import ap404.xclone.Shared.Models.Tweet;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class ExploreController
+{
+    @FXML private VBox tweetContainer;
+
+    @FXML
+    public void initialize() { loadTweets(); }
+
+    private void loadTweets()
+    {
+        try
+        {
+            TweetUtil.addTweet(tweetContainer, new Tweet("Ali", "@ali", "Welcome to explore"));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
