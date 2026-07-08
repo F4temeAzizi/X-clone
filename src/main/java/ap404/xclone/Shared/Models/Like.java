5bd6b55e32e44972f03a575097699de3363a6083
Fateme Azizi <azizifateme423@gmail.com>
2026-07-08 22:25:25 +0330

    feat: implement Like.java

---
+++ src/main/java/ap404/xclone/Shared/Models/Like.java
@@ -0,0 +1,20 @@
+package ap404.xclone.Shared.Models;
+
+import java.security.Timestamp;
+
+public class Like {
+
+    private int userId;
+    private int tweetId;
+    private Timestamp createdAt;
+
+    public void Like(int userId, int tweetId, Timestamp createdAt) {
+        this.userId = userId;
+        this.tweetId = tweetId;
+        this.createdAt = createdAt;
+    }
+
+    public int getUserId() { return userId; }
+    public int getTweetId() { return tweetId; }
+    public Timestamp getCreatedAt() { return createdAt; }
+}
