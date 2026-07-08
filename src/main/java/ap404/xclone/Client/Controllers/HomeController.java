5115e6e5cb00b703c12168d95436b1c692b101cb
Bita <bitahajati1386@gmail.com>
2026-07-08 20:15:14 +0330

    feat(explore): add TweetUtil.java

--- src/main/java/ap404/xclone/Client/Controllers/HomeController.java
+++ src/main/java/ap404/xclone/Client/Controllers/HomeController.java
@@ -1,6 +1,7 @@
 package ap404.xclone.Client.Controllers;
 
 import ap404.xclone.Client.Managers.Navigation;
+import ap404.xclone.Client.Utils.TweetUtil;
 import ap404.xclone.Client.Utils.UserUtil;
 import ap404.xclone.Shared.Models.Tweet;
 import javafx.fxml.FXML;
@@ -19,27 +20,14 @@ public class HomeController
     {
         Navigation.setHomeController(this);
         updateComposeAvatar();
-
         try
         {
-            addTweet(new Tweet("Ali", "@ali", "Hello World!"));
+            TweetUtil.addTweet(tweetContainer, new Tweet("Ali", "@ali", "Hello World!"));
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-    }
-
-    private void addTweet(Tweet tweet) throws Exception
-    {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Tweet.fxml"));
-
-        HBox root = loader.load();
-
-        TweetController controller = loader.getController();
-        controller.setTweet(tweet);
-
-        tweetContainer.getChildren().add(root);
     }
 
     public void updateComposeAvatar ()
