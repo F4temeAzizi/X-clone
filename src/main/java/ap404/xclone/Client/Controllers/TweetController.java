800360ad5dfb27d5e8c1484859c81d58e3afd99a
Fateme Azizi <azizifateme423@gmail.com>
2026-07-09 02:45:59 +0330

    fix: restore like state when loading tweets

--- src/main/java/ap404/xclone/Client/Controllers/TweetController.java
+++ src/main/java/ap404/xclone/Client/Controllers/TweetController.java
@@ -2,26 +2,28 @@ package ap404.xclone.Client.Controllers;
 
 import ap404.xclone.Client.Client;
 import ap404.xclone.Client.Managers.Session;
+import ap404.xclone.Server.Database.LikeDao;
 import ap404.xclone.Shared.DTO.enums.RequestType;
 import ap404.xclone.Shared.DTO.enums.ResponseType;
 import ap404.xclone.Shared.DTO.request.LikeRequest;
 import ap404.xclone.Shared.DTO.request.Request;
-import ap404.xclone.Shared.DTO.response.Response;
 import ap404.xclone.Shared.Models.Tweet;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 
 public class TweetController
 {
+    @FXML private Label likeCountLabel;
     @FXML private Button likeBtn;
     @FXML private Label nameLabel;
     @FXML private Label usernameLabel;
     @FXML private Label contentLabel;
     private int tweetId;
+    private LikeDao likeDao = new LikeDao();
+
 
     public void setTweet(Tweet tweet)
     {
@@ -29,6 +31,10 @@ public class TweetController
         nameLabel.setText(tweet.getName());
         usernameLabel.setText(tweet.getUsername());
         contentLabel.setText(tweet.getContent());
+
+        if (likeDao.isLiked(Session.getCurrentUser().getId(), tweetId)) {
+            likeBtn.getStyleClass().add("liked");
+        }
     }
 
     public void like() throws IOException, ClassNotFoundException {
