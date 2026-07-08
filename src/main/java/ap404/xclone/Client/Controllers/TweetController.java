fdec8c5f2af76e02228cccad8d6846f2592a7435
Bita <bitahajati1386@gmail.com>
2026-07-09 00:02:25 +0330

    feat(profile): display tweet creation time

--- src/main/java/ap404/xclone/Client/Controllers/TweetController.java
+++ src/main/java/ap404/xclone/Client/Controllers/TweetController.java
@@ -6,12 +6,19 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TweetController
 {
     @FXML private Label nameLabel;
     @FXML private Label usernameLabel;
     @FXML private Label contentLabel;
     @FXML private ImageView avatarImage;
+    @FXML private Label timeLabel;
 
     public void setTweet(Tweet tweet)
     {
@@ -20,5 +27,20 @@ public class TweetController
         contentLabel.setText(tweet.getContent());
 
         if (tweet.getAvatarImageUrl() != null) avatarImage.setImage(new Image(tweet.getAvatarImageUrl()));
+        timeLabel.setText(formatTime(tweet.getCreatedAt()));
+    }
+
+    private String formatTime(Timestamp timestamp)
+    {
+        LocalDateTime tweetTime = timestamp.toLocalDateTime();
+        LocalDateTime now = LocalDateTime.now();
+
+        Duration duration = Duration.between(tweetTime, now);
+
+        if (duration.toMinutes() < 1) return "now";
+        if (duration.toHours() < 1) return  duration.toMinutes() + "m";
+        if (duration.toDays() < 1) return duration.toHours() + "h";
+        if (tweetTime.getYear() == now.getYear()) return tweetTime.format(DateTimeFormatter.ofPattern("MMM d"));
+        else return tweetTime.format(DateTimeFormatter.ofPattern("MMM d, yyyy"));
     }
 }
