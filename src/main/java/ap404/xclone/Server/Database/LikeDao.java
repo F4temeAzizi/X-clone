d1bb0d7b0ca35b1ff4748452b66d9cf5d1ea62fa
Fateme Azizi <azizifateme423@gmail.com>
2026-07-09 00:14:54 +0330

    feat: remove unliked Like from database

--- src/main/java/ap404/xclone/Server/Database/LikeDao.java
+++ src/main/java/ap404/xclone/Server/Database/LikeDao.java
@@ -27,4 +27,25 @@ public class LikeDao {
             return false;
         }
     }
+
+    public boolean unlikeTweet(int userId, int tweetId) {
+
+        String sql = """
+                DELETE FROM likes 
+                WHERE user_id = ? AND tweet_id = ?
+                """;
+
+        try(Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, userId);
+            statement.setInt(2, tweetId);
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            System.out.println("unlike error: " + e.getMessage());
+            return false;
+        }
+    }
 }
