003a3b3ebf05986498282d1f062ad61abd57b3a8
Fateme Azizi <azizifateme423@gmail.com>
2026-07-08 23:27:17 +0330

    feat: add Client to Session.java

--- src/main/java/ap404/xclone/Client/Managers/Session.java
+++ src/main/java/ap404/xclone/Client/Managers/Session.java
@@ -1,10 +1,15 @@
 package ap404.xclone.Client.Managers;
 
+import ap404.xclone.Client.Client;
 import ap404.xclone.Shared.Models.User;
 
 public class Session
 {
     private static User currentUser;
+    private static Client client;
+
+    public static Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client;}
 
     public static User getCurrentUser() { return currentUser; }
 
