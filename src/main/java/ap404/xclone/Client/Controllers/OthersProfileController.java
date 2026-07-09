a515a57d699fdb942606046305816297504b1d6a
Bita <bitahajati1386@gmail.com>
2026-07-09 13:03:13 +0330

    feat(profile): add OthersProfileController.java

---
+++ src/main/java/ap404/xclone/Client/Controllers/OthersProfileController.java
@@ -0,0 +1,23 @@
+package ap404.xclone.Client.Controllers;
+
+import ap404.xclone.Client.Managers.Navigation;
+import ap404.xclone.Client.Utils.UserUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+
+public class OthersProfileController
+{
+    @FXML private Label nameLbl;
+    @FXML private Label usernameLbl;
+    @FXML private Label bioLbl;
+    @FXML private ImageView avatarImage;
+    @FXML private Region bannerRegion;
+    @FXML private Label createdAtLbl;
+
+    public void initialize ()
+    {
+        UserUtil.loadUser(Navigation.getSelectedUser(), nameLbl, usernameLbl, bioLbl, avatarImage, bannerRegion, createdAtLbl);
+    }
+}
