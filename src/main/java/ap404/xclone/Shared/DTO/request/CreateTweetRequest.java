cac5090656a100d5e7a33b0130f5dffbb42eac00
Bita <bitahajati1386@gmail.com>
2026-07-16 02:18:10 +0330

    feat(media): support media attachments in create tweet request

--- src/main/java/ap404/xclone/Shared/DTO/request/CreateTweetRequest.java
+++ src/main/java/ap404/xclone/Shared/DTO/request/CreateTweetRequest.java
@@ -1,21 +1,26 @@
 package ap404.xclone.Shared.DTO.request;
 
+import ap404.xclone.Shared.Models.Media;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class CreateTweetRequest implements Serializable {
     private int userId;
     private String content;
+    private List<Media> media;
 
-    public CreateTweetRequest(int userId, String content) {
+    public CreateTweetRequest(int userId, String content, List<Media> media) {
         this.userId = userId;
         this.content = content;
+        this.media = media;
     }
 
     public int getUserId() {
         return userId;
     }
-
     public String getContent() {
         return content;
     }
+    public List<Media> getMedia() { return media; }
 }
