2f937387f288e9d4a68cd17269f83ae051bd3402
Bita <bitahajati1386@gmail.com>
2026-07-19 01:40:03 +0330

    style(media): round media layout

--- src/main/java/ap404/xclone/Client/Utils/MediaUtil.java
+++ src/main/java/ap404/xclone/Client/Utils/MediaUtil.java
@@ -10,6 +10,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -29,43 +30,43 @@ public class MediaUtil
         if (count == 0) return;
         if (count == 1)
         {
-            pane.getChildren().add(createItem(media.get(0), media, pane, 650, 420, true));
+            pane.getChildren().add(rounded(createItem(media.get(0), media, pane, 650, 420, true)));
         }
         else if (count == 2)
         {
-            HBox r = new HBox(5);
-            r.getChildren().add(createItem(media.get(0), media, pane, 322,  420, false));
-            r.getChildren().add(createItem(media.get(1), media, pane, 322,  420, false));
-            pane.getChildren().add(r);
+            HBox r = new HBox(1);
+            r.getChildren().add(createItem(media.get(0), media, pane, 324,  420, false));
+            r.getChildren().add(createItem(media.get(1), media, pane, 324,  420, false));
+            pane.getChildren().add(rounded(r));
         }
         else if (count == 3)
         {
-            VBox root = new VBox(5);
-            root.getChildren().add(createItem(media.get(0), media, pane, 650, 210, false));
+            VBox root = new VBox(1);
+            root.getChildren().add(createItem(media.get(0), media, pane, 649, 210, false));
 
-            HBox r = new HBox(5);
-            r.getChildren().add(createItem(media.get(1), media, pane, 322, 205, false));
-            r.getChildren().add(createItem(media.get(2), media, pane, 322, 205, false));
+            HBox r = new HBox(1);
+            r.getChildren().add(createItem(media.get(1), media, pane, 324, 205, false));
+            r.getChildren().add(createItem(media.get(2), media, pane, 324, 205, false));
 
             root.getChildren().add(r);
-            pane.getChildren().add(root);
+            pane.getChildren().add(rounded(root));
         }
         else
         {
-            VBox root = new VBox(5);
+            VBox root = new VBox(1);
 
-            HBox r1 = new HBox(5);
-            HBox r2 = new HBox(5);
+            HBox r1 = new HBox(1);
+            HBox r2 = new HBox(1);
 
-            r1.getChildren().add(createItem(media.get(0), media, pane, 322, 205, false));
-            r1.getChildren().add(createItem(media.get(1), media, pane, 322, 205, false));
+            r1.getChildren().add(createItem(media.get(0), media, pane, 324, 205, false));
+            r1.getChildren().add(createItem(media.get(1), media, pane, 324, 205, false));
 
-            r2.getChildren().add(createItem(media.get(2), media, pane, 322, 205, false));
-            r2.getChildren().add(createItem(media.get(3), media, pane, 322, 205, false));
+            r2.getChildren().add(createItem(media.get(2), media, pane, 324, 205, false));
+            r2.getChildren().add(createItem(media.get(3), media, pane, 324, 205, false));
 
             root.getChildren().addAll(r1, r2);
 
-            pane.getChildren().add(root);
+            pane.getChildren().add(rounded(root));
         }
     }
 
@@ -97,7 +98,7 @@ public class MediaUtil
 
             previewPlayer.setMute(true);
             previewPlayer.setAutoPlay(true);
-            
+
             StackPane videoPane = new StackPane(mediaView);
             Label play = new Label("▶");
 
@@ -143,42 +144,42 @@ public class MediaUtil
         if (count == 0) return;
         if (count == 1)
         {
-            pane.getChildren().add(createTweetItem(media.get(0), 650, 420, true));
+            pane.getChildren().add(rounded(createTweetItem(media.get(0), 650, 420, true)));
         }
         else if (count == 2)
         {
-            HBox row = new HBox(5);
-            row.getChildren().add(createTweetItem(media.get(0), 322, 420, false));
-            row.getChildren().add(createTweetItem(media.get(1), 322, 420, false));
-            pane.getChildren().add(row);
+            HBox r = new HBox(1);
+            r.getChildren().add(createTweetItem(media.get(0), 324, 420, false));
+            r.getChildren().add(createTweetItem(media.get(1), 324, 420, false));
+            pane.getChildren().add(rounded(r));
         }
         else if (count == 3)
         {
-            VBox root = new VBox(5);
-            root.getChildren().add(createTweetItem(media.get(0), 650, 210, false));
+            VBox root = new VBox(1);
+            root.getChildren().add(createTweetItem(media.get(0), 649, 210, false));
 
-            HBox r = new HBox(5);
-            r.getChildren().add(createTweetItem(media.get(1), 322, 205, false));
-            r.getChildren().add(createTweetItem(media.get(2), 322, 205, false));
+            HBox r = new HBox(1);
+            r.getChildren().add(createTweetItem(media.get(1), 324, 205, false));
+            r.getChildren().add(createTweetItem(media.get(2), 324, 205, false));
 
             root.getChildren().add(r);
-            pane.getChildren().add(root);
+            pane.getChildren().add(rounded(root));
         }
         else
         {
-            VBox root = new VBox(5);
+            VBox root = new VBox(1);
 
-            HBox r1 = new HBox(5);
-            HBox r2 = new HBox(5);
+            HBox r1 = new HBox(1);
+            HBox r2 = new HBox(1);
 
-            r1.getChildren().add(createTweetItem(media.get(0), 322, 205, false));
-            r1.getChildren().add(createTweetItem(media.get(1), 322, 205, false));
+            r1.getChildren().add(createTweetItem(media.get(0), 324, 205, false));
+            r1.getChildren().add(createTweetItem(media.get(1), 324, 205, false));
 
-            r2.getChildren().add(createTweetItem(media.get(2), 322, 205, false));
-            r2.getChildren().add(createTweetItem(media.get(3), 322, 205, false));
+            r2.getChildren().add(createTweetItem(media.get(2), 324, 205, false));
+            r2.getChildren().add(createTweetItem(media.get(3), 324, 205, false));
 
             root.getChildren().addAll(r1, r2);
-            pane.getChildren().add(root);
+            pane.getChildren().add(rounded(root));
         }
     }
 
@@ -321,6 +322,22 @@ public class MediaUtil
             currentPlayer = null;
         });
         stage.showAndWait();
+    }
+
+    private static StackPane rounded(Pane pane)
+    {
+        StackPane stackPane = new StackPane(pane);
+
+        Rectangle clip = new Rectangle();
+        clip.setArcHeight(20);
+        clip.setArcWidth(20);
+
+        clip.widthProperty().bind(stackPane.widthProperty());
+        clip.heightProperty().bind(stackPane.heightProperty());
+
+        stackPane.setClip(clip);
+
+        return stackPane;
     }
 }
 
