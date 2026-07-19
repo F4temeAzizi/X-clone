5ea540004377791e84294c9391fa9c66f1f2c661
Fateme Azizi <azizifateme423@gmail.com>
2026-07-19 20:59:18 +0330

    feat(reply): add content field to ReplyRequest.java

--- src/main/java/ap404/xclone/Shared/DTO/request/ReplyRequest.java
+++ src/main/java/ap404/xclone/Shared/DTO/request/ReplyRequest.java
@@ -6,13 +6,17 @@ public class ReplyRequest implements Serializable {
 
     private int userId;
     private int tweetId;
+    private String content;
 
-    public ReplyRequest(int userId, int tweetId) {
+    public ReplyRequest(int userId, int tweetId, String content) {
         this.userId = userId;
         this.tweetId = tweetId;
+        this.content = content;
     }
 
     public int getUserId() { return userId; }
 
     public int getTweetId() { return tweetId; }
+
+    public String getContent() { return content; }
 }
