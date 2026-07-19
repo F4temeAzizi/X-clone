92bcddcea16f9ecd617ebfbbd08f754284c71980
Fateme Azizi <azizifateme423@gmail.com>
2026-07-20 00:59:19 +0330

    feat(reply): implement ReplyController.java

---
+++ src/main/java/ap404/xclone/Client/Controllers/ReplyController.java
@@ -0,0 +1,58 @@
+package ap404.xclone.Client.Controllers;
+
+import ap404.xclone.Client.Utils.TweetUtil;
+import ap404.xclone.Shared.Models.Tweet;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ReplyController {
+
+    @FXML private Label charCount;
+    @FXML private Button replyBtn;
+    @FXML private TextArea replyArea;
+    @FXML private VBox tweetContainer;
+    @FXML private Label replyingToLabel;
+
+    private static final int MAX_REPLY_LENGTH = 280;
+    private Tweet tweet;
+    private String text;
+
+    @FXML
+    public void initialize() {
+
+        replyBtn.setDisable(true);
+        charCount.setText("0/" + MAX_REPLY_LENGTH);
+
+        replyArea.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            int length = newValue.length();
+
+            charCount.setText(length + "/" + MAX_REPLY_LENGTH);
+
+            replyBtn.setDisable(newValue.isBlank() || length > MAX_REPLY_LENGTH);
+        });
+    }
+
+    public void setTweet(Tweet tweet) {
+        this.tweet = tweet;
+
+        replyingToLabel.setText("Replying to @" + tweet.getUsername());
+
+        try {
+            TweetUtil.addTweet(tweetContainer, tweet);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void reply() {
+        text = replyArea.getText().trim();
+        ((Stage) replyArea.getScene().getWindow()).close();
+    }
+
+    public String getReplyText() { return text; }
+}
